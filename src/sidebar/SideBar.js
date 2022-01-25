f67de7fcf45e66458f3694fb6ab19806be50db10
hackhim18 <triphimanshu18@gmail.com>
2022-01-25 21:54:44 +0530

    c8-25/01

--- src/sidebar/SideBar.js
+++ src/sidebar/SideBar.js
@@ -14,7 +14,7 @@ export default function Sidebar() {
           Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
           amet ex esse.Sunt eu ut nostrud id quis proident.
         </p>
-      
+        </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
@@ -34,7 +34,7 @@ export default function Sidebar() {
           <i className="sidebarIcon fab fa-pinterest-square"></i>
           <i className="sidebarIcon fab fa-twitter-square"></i>
         </div>
-      </div>
+     
     </div>
     </div>
   );
