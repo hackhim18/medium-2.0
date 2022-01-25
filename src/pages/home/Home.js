3698d94f13ba2794479b128d913a64454ea16bf2
hackhim18 <triphimanshu18@gmail.com>
2022-01-25 12:58:06 +0530

    c3-25/01

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Posts from "../../posts/Posts"
 import Header from '../../header/Header';
+import Sidebar from '../../sidebar/SideBar';
 import "./home.css"
 
 export default function Home() {
-  return <div className='home'>
-      <Header /> 
-Homepage
-  </div>;
+ return(
+   <>
+   <Header />
+   <div className='home'> 
+   <Posts />
+   <Sidebar />
+   </div>
+   </>
+ )
 }
